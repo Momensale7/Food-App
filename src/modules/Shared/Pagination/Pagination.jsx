fc2cc59ef816b1e1d22acdbd14de26a3e95536b8
momen <momensalah2468@gmail.com>
2025-03-12 03:06:35 +0200

    reusable pagination ui in small screen

--- src/modules/Shared/Pagination/Pagination.jsx
+++ src/modules/Shared/Pagination/Pagination.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Pagination({pageNumber, handlePageChange, totalPages}) {
   return (
     <>
-       <nav aria-label="Page navigation example">
+       <nav aria-label="Page navigation example mt-2">
   <ul className="pagination justify-content-center">
     {/* Previous Button */}
     <li className={`page-item ${pageNumber === 1 ? 'disabled' : ''}`}>
@@ -12,37 +12,37 @@ export default function Pagination({pageNumber, handlePageChange, totalPages}) {
       </button>
     </li>
 
-    {/* First Page (Always Visible) */}
-    <li className={`page-item ${pageNumber === 1 ? 'active' : ''}`}>
+    {/* First Page */}
+    <li className={`d-none d-lg-block  page-item ${pageNumber === 1 ? 'active' : ''}`}>
       <button className="page-link" onClick={() => handlePageChange(1)}>1</button>
     </li>
 
     {/* Left Ellipsis */}
     {pageNumber > 6 && (
-      <li className="page-item disabled">
+      <li className="d-none d-lg-block  page-item disabled">
         <span className="page-link">...</span>
       </li>
     )}
 
-    {/* Dynamic Page Numbers (Excluding First and Last Page) */}
+    {/* Dynamic Page Numbers*/}
     {Array.from({ length: totalPages }, (_, i) => i + 1)
       .filter((num) => num !== 1 && num !== totalPages && num >= pageNumber - 3 && num <= pageNumber + 3)
       .map((num) => (
-        <li key={num} className={`page-item ${pageNumber === num ? 'active' : ''}`}>
+        <li key={num} className={`d-none d-lg-block  page-item ${pageNumber === num ? 'active' : ''}`}>
           <button className="page-link" onClick={() => handlePageChange(num)}>{num}</button>
         </li>
       ))}
 
     {/* Right Ellipsis */}
     {pageNumber < totalPages - 3 && (
-      <li className="page-item disabled">
+      <li className="d-none d-lg-block  page-item disabled">
         <span className="page-link">...</span>
       </li>
     )}
 
-    {/* Last Page (Always Visible) */}
+    {/* Last Page */}
     {totalPages > 1 && (
-      <li className={`page-item ${pageNumber === totalPages ? 'active' : ''}`}>
+      <li className={`d-none d-lg-block  page-item ${pageNumber === totalPages ? 'active' : ''}`}>
         <button className="page-link" onClick={() => handlePageChange(totalPages)}>{totalPages}</button>
       </li>
     )}
