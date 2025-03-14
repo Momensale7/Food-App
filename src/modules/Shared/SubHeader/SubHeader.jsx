2ecc6281e8aeba7efcd4dbbf6df298551f3cb14a
momen <momensalah2468@gmail.com>
2025-03-14 23:47:04 +0200

    modify subheader

--- src/modules/Shared/SubHeader/SubHeader.jsx
+++ src/modules/Shared/SubHeader/SubHeader.jsx
@@ -14,10 +14,12 @@ export default function SubHeader({ title, description, btnContent, getCategorie
             <p className='fs-12 mt-0'>{description}</p>
           </div>
           {btnContent &&
-          routeTo?
+          <>
+          {routeTo?
             <Link to={routeTo} className='btn greenMainBg px-3 fs-6 fw-bold py-1 text-white'>{btnContent}</Link>
             :<button onClick={() => setShowModal(true)} className='btn greenMainBg px-3 fs-6 fw-bold py-1 text-white'>{btnContent}</button>
           }
+          </>}
         </div>
       </div>
       <CategoriesData type={'add'} show={showModal} onClose={() => setShowModal(false)} getCategories={getCategories}/>
